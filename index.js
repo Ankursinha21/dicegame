45bb194bd41f874587bee00d1ebd3c2aa1be5f71
Ankursinha21 <89289480+Ankursinha21@users.noreply.github.com>
2023-02-22 12:42:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function buttonClick() {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  let randomDiceImage = "dice" + randomNumber1 + ".png";
+
+  let randomImageSource = "images/" + randomDiceImage;
+
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h2").innerHTML = "Player 1 Wins";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h2").innerHTML = "Player 2 Wins";
+  } else {
+    document.querySelector("h2").innerHTML = "It's A Tie!!!";
+  }
+}
